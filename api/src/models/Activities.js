a5174aa28f19281c22cf1574820bdb42cd02d401
Mek <99274729+mgs1987@users.noreply.github.com>
2023-06-06 14:06:23 -0400

    lasts

--- api/src/models/Activities.js
+++ api/src/models/Activities.js
@@ -1,28 +1,27 @@
 const sequelize = require("sequelize");
-const { DataTypes } = require("sequelize")
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('Activities', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            unique:true,
-            primaryKey: true
-        },
-        name: {
-           type: DataTypes.STRING,
-        },
-        difficulty: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        duration: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        season: {
-            type: DataTypes.ENUM("Winter", "Summer","Autumn","Spring")
-        },
+  sequelize.define("Activities", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+      primaryKey: true,
     },
-    )};
-
+    name: {
+      type: DataTypes.STRING,
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    season: {
+      type: DataTypes.ENUM("Winter", "Summer", "Autumn", "Spring"),
+    },
+  });
+};
